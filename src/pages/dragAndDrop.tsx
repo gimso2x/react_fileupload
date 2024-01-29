1d35211fea6681ba733091ee078e53ac3301638f
seonggwan kim <sgkim@ekms.kr>
2024-01-29 13:44:24 +0900

    first commit

---
+++ src/pages/dragAndDrop.tsx
@@ -0,0 +1,138 @@
+import { useCallback, useRef, useState, useEffect } from "react";
+import styles from "./dragAndDrop.module.css";
+
+const DragAndDrop = () => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [files, setFiles] = useState([]);
+
+  const dragRef = useRef<HTMLLabelElement | null>(null);
+  const fileId = useRef(0);
+  console.log(files);
+  const onChangeFiles = useCallback(
+    (e) => {
+      let selectFiles = [];
+      let tempFiles: { id: number; object: File }[] = files;
+
+      if (e.type === "drop") {
+        selectFiles = e.dataTransfer.files;
+      } else {
+        selectFiles = e.target.files;
+      }
+
+      for (const file of selectFiles) {
+        tempFiles = [
+          ...tempFiles,
+          {
+            id: fileId.current++,
+            object: file,
+          },
+        ];
+      }
+
+      setFiles(tempFiles);
+    },
+    [files]
+  );
+
+  const handleFilterFile = useCallback(
+    (id) => {
+      setFiles(files.filter((file) => file.id !== id));
+    },
+    [files]
+  );
+
+  const handleDragIn = useCallback((e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }, []);
+
+  const handleDragOut = useCallback((e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setIsDragging(false);
+  }, []);
+
+  const handleDragOver = useCallback((e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (e.dataTransfer!.files) {
+      setIsDragging(true);
+    }
+  }, []);
+
+  const handleDrop = useCallback(
+    (e: DragEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      onChangeFiles(e);
+      setIsDragging(false);
+    },
+    [onChangeFiles]
+  );
+
+  const initDragEvents = useCallback(() => {
+    if (dragRef.current !== null) {
+      dragRef.current.addEventListener("dragenter", handleDragIn);
+      dragRef.current.addEventListener("dragleave", handleDragOut);
+      dragRef.current.addEventListener("dragover", handleDragOver);
+      dragRef.current.addEventListener("drop", handleDrop);
+    }
+  }, [handleDragIn, handleDragOut, handleDragOver, handleDrop]);
+
+  const resetDragEvents = useCallback(() => {
+    if (dragRef.current !== null) {
+      dragRef.current.removeEventListener("dragenter", handleDragIn);
+      dragRef.current.removeEventListener("dragleave", handleDragOut);
+      dragRef.current.removeEventListener("dragover", handleDragOver);
+      dragRef.current.removeEventListener("drop", handleDrop);
+    }
+  }, [handleDragIn, handleDragOut, handleDragOver, handleDrop]);
+
+  useEffect(() => {
+    initDragEvents();
+
+    return () => resetDragEvents();
+  }, [initDragEvents, resetDragEvents]);
+
+  return (
+    <div>
+      <input
+        type="file"
+        id="fileUpload"
+        style={{ display: "none" }}
+        multiple={true}
+        onChange={onChangeFiles}
+      />
+
+      <label
+        className={`${styles.wrap} ${isDragging ? "dragging" : "dropped"}`}
+        htmlFor="fileUpload"
+        ref={dragRef}
+      >
+        <div>파일 첨부</div>
+      </label>
+
+      <div className={styles.ul}>
+        {files.length > 0 &&
+          files.map((file) => {
+            const {
+              id,
+              object: { name },
+            } = file;
+
+            return (
+              <div key={id} className={styles.li}>
+                <div>{name}</div>
+                <div onClick={() => handleFilterFile(id)}>파일삭제</div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default DragAndDrop;
